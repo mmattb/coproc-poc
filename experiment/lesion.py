1515b6aa6181d1e2fcb0eef36aac918bf968ee86
mbryan <matthew.bryan@dell.com>
2021-11-11 13:00:58 -0800

    Paper progress

--- experiment/lesion.py
+++ experiment/lesion.py
@@ -71,6 +71,19 @@ class LesionOutputsByIdxs(Lesion):
     def __str__(self):
         return f"outputsIdxs{self.start_idx}.{self.end_idx}"
 
+class LesionOutputsByMask(Lesion):
+    def __init__(self, num_neurons_per_module, lesion_mask, cuda=None):
+        assert lesion_mask.shape == (num_neurons_per_module * 3,)
+        self.lesion_mask = lesion_mask
+
+    def lesion(self, network, pre_response):
+        batch_size = pre_response.shape[0]
+        out = pre_response * torch.tile(self.lesion_mask, (batch_size, 1))
+        return out
+
+    def __str__(self):
+        return f"outputsIdxs{self.start_idx}.{self.end_idx}"
+
 
 class LesionConnectionsByIdxs(Lesion):
     application = "connection"
